387c41806f9ac4dedaf46e183763a022adea3229
Krish Agarwalla <152370368+krish-agarwalla@users.noreply.github.com>
2026-01-12 01:22:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-const introVideo = document.getElementById("introVideo");
-const bgDesktop = document.getElementById("bgDesktop");
-const bgMobile = document.getElementById("bgMobile");
-const skipBtn = document.getElementById("skipIntro");
+const menuBtn = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
 
-if (introVideo) {
-  introVideo.playbackRate = 0.75;
-
-  function showMain() {
-    document.querySelector(".intro-video").style.display = "none";
-    bgDesktop.classList.remove("hidden");
-    bgMobile.classList.remove("hidden");
-  }
-
-  introVideo.addEventListener("ended", showMain);
-
-  if (skipBtn) {
-    skipBtn.addEventListener("click", showMain);
-  }
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+  });
 }
