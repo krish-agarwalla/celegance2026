a4d20b7d9046364d8d1f9293537511edee02796a
Krish Agarwalla <152370368+krish-agarwalla@users.noreply.github.com>
2026-01-12 10:23:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Mobile Menu */
 const menuBtn = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobileMenu");
 
@@ -5,4 +6,21 @@ if (menuBtn) {
   menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("show");
   });
+}
+
+/* Close menu on link click */
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+  });
+});
+
+/* Toast Message */
+function showToast() {
+  const toast = document.getElementById("toast");
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 8000);
 }
